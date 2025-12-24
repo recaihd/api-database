5828bb6d8fd597cbbe954676005a39ad777d5af7
recaihd <ricardosousapixtudo@gmail.com>
2025-12-24 17:08:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static('.'));
+
+mongoose.connect(process.env.MONGO_URI);
+
+const Esquema = mongoose.model('Mensagens', {texto: String});
+
+app.post('/enviar', async (req, res) => {
+    try {
+        await new Esquema({ texto: req.body.texto }).save();
+res.json({ok: true});
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('api rodando em http:localhost:3000'))
