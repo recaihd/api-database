358da4519b45006a54a37d544ef0f53b616a1faa
recaihd <ricardosousapixtudo@gmail.com>
2025-12-27 10:40:54 -0300

    saos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+// sexo
 const app = express();
 app.use(express.json());
 app.use(cors());
